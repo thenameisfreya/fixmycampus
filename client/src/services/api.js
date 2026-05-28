6cdaf5c142095d927e526ac46fd9fff01319ebd6
thenameisfreya <fareehazasad@gmail.com>
2026-05-28 23:02:52 +0100

    Use an environment variable in api.js for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: 'http://localhost:5001/api' 
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001/api' 
 });
 
 API.interceptors.request.use((config) => {
